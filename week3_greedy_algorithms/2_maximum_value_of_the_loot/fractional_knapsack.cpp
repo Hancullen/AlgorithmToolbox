7201485431d23da7c3d62e8fa4c964664d8fd1e0
Hancullen <linhnguyen960506@gmail.com>
2019-07-23 00:43:51 +0300

    approach step 1

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstdlib>
+// #include <iomanip>
 
 using std::vector;
 
-double get_optimal_value(int capacity, vector<int>& weights, vector<int>& values) {
-  int v = values.size();
-  int w = weights.size();
-  double value_per_unit = 0.0;
-  int backpack[v][w];
-  for(int i=0; i <= v; ++i){
-    // value_per_unit = values[i]/weights[i];
-    for(int j=0; j <= capacity; ++j){
-
-      if((i == 0) || j == 0) //base case(when backpack is full)
-        return 0;
-      else if(weights[i] <= j){
-        backpack[i][j] = std::max(values[i]+backpack[i-1][j-weights[i]], backpack[i-1][j]);
-      }
-      else{
-        backpack[i][j] = backpack[i+1][j];
-      }
-      return backpack[i][j];
-    }
+// get index of the item in the list whose value is the highest
+int get_max_value(vector<int> w, vector<int> v){
+  int max_index = 0;
+  double max_value = 0.0;
+  for(int i=0; i<= w.size(); ++i){
+    if(w[i] > 0 && ((double)v[i]/w[i] > max_value)){
+      max_value = (double) v[i]/w[i];
+      max_index = i;
+    } 
   }
+  return max_index; //return index = position of items
+}
+
+double get_optimal_value(int capacity, vector<int>& weights, vector<int>& values) {
+  double value = 0.0;
+
+  for(int i=0; i <= weights.size(); ++i){
+      if(capacity == 0) 
+        return value;
+      else{
+        int index = get_max_value(weights, values); // get the position of item
+        //check if the weight of that item is still within capacity and take allowance kg of that item
+        int amount = std::min(capacity, weights[index]);
+        value += amount * (double) values[index]/weights[index];
+        capacity -= amount; //remaining of the capacity
+        weights[index] -= amount; //remaining of the item
+      }
+  }
+  return value;
 }
 
 int main() {
@@ -39,7 +48,7 @@ int main() {
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
+  std::cout.precision(10); //std::cout << std::setprecision(5) << optimal_value << '\n';
   std::cout << optimal_value << std::endl;
   return 0;
 }
